14f6fb5b1cf108693f11aad4af8c7a02a1b6c8fc
dgbragas <dgbragas.dev@outlook.com>
2020-07-26 16:59:49 -0300

    Adding global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+import githubBackground from '../assets/github-background.svg';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  body {
+    background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  #root {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 40px 20px;
+  }
+`;
